53edcfdf9824dd3bfd1871759ec86d8a37b5c874
Guilherme-NascimentoSantos <guilherme.nascimento@bandtec.com.br>
2020-09-17 12:35:27 -0300

    Novo banco

--- Banco-PI.sql
+++ Banco-PI.sql
@@ -1,41 +1,67 @@
-create database controladora_sensores;
+create database sustabil;
 
-use controladora_sensores;
+use sustabil;
 
 create table tbclientes(
 
-idCliente int not null auto_increment primary key,
-NomeCliente varchar(80) not null,
-emailCliente varchar(50) not null,
-CPFCliente char(15) not null,
-RGCliente char(12) not null,
-EndCliente varchar(80) not null,
-CEPCliente char(9) not null,
+idCliente int primary key,
+NomeCliente varchar(80),
+emailCliente varchar(50),
+CPFCliente char(15),
+RGCliente char(12),
+EndCliente varchar(80),
+CEPCliente char(9),
 EmpresaCliente varchar(50)
 );
 
 create table tbRua(
 
-idRua int not null auto_increment primary key,
-EndRua varchar(80) not null,
+idRua int primary key,
+EndRua varchar(80),
 nomeRoteador varchar(20)
 );
 
 create table tbsensores(
 
-idSensor int not null auto_increment primary key,
-MediaSensor int not null,
-AlertaSensor char(3),
-localSensor varchar(80),
-idCliente int not null,
-idRua int not null,
-foreign key (idCliente) references tbclientes(idCliente),
-foreign key (idRua) references tbRua(idRua)
+idSensor int primary key,
+MinUmidade int,
+MaxUmidade int,
+MediaSensor int,
+AlertaSensor varchar(20)
 );
 
-desc tbSensores;
+insert into tbclientes
+values(1,'Anderson Mariano','anderson@bandtec.com','343.953.940-23','88.932.032-3','Rua Anderson de souza N° 90','34234-998','Sabesp'),
+(2,'Arthur de Paula','arthur@bandtec.com','032.990.232-93','32.990.332-2','Rua Anderson de souza N° 90','94803-239','Sabesp'),
+(3,'Franklin da Silva','franklin@bandtec.com','093.099.232-01','98.323.098-5','Rua Franklin gerivaldo N°93','38490-974','Ministério do Meio Ambiente'),
+(4,'Gabriel Ferraz','gabriel@bandtec.com','832.993.934-33','93.893.865-8','Rua Franklin gerivaldo N°93','37402-342','Ministério do Meio Ambiente'),
+(5,'Guilherme Nascimento','guilherme@bandtec.com','324.974.088-93','34.976.903-5','Rua Guilherme gildo N°39','23909-787','Ministério do Desenvolvimento Regional'),
+(6,'João Oliveira','joao@bandtec.com','394.903.232-94','35.868.999-3','Rua Guilherme gildo N°39','89034-234','Ministério do Desenvolvimento Regional');
+
+insert into tbrua
+values(1,'Rua Alfredo silva','Roteador-Alfredo'),
+(2,'Rua Josivaldo Souza','Roteador-Josivaldo'),
+(3,'Rua Astrogildo Faria','Roteador-Astrogildo'),
+(4,'Rua Josislene','Roteador-Josislene'),
+(5,'Rua Miguelito','Roteador-Miguelito'),
+(6,'Rua Altervildo','Roteador-Altervido');
+
+insert into tbsensores
+values(1,40,80,57,'Normal'),
+(2,30,70,40,'Não Passou'),
+(3,40,80,80,'Ultrapassou'),
+(4,50,90,30,'Muito abaixo'),
+(5,40,80,50,'Normal'),
+(6,50,90,80,'Normal');
+
 
 -- drop database controladora_sensores;
+
+select * from tbclientes;
+
+select * from tbrua;
+
+select * from tbsensores;
 
 
 
